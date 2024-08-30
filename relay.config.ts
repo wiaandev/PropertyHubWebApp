dad40fff7962b15fad1751c7e710466f6118ec2e
Wiaan Duvenhage <wiaanduvenhage.dev@gmail.com>
2024-08-30 13:41:13 +0200

    chore: configured relay

---
+++ relay.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+    // ...
+    // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
+    src: "./src",
+    language: "typescript", // "javascript" | "typescript" | "flow"
+    schema: "./src//schema.graphql",
+    excludes: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
+  }
